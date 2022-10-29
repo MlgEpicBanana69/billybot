cff5d89429533d374c5f80428300993c0c76d882
Tamir Albilia <mlgepicbanana69@gmail.com>
2022-10-29 22:28:59 +0000

    Revert "Merge branch 'main' of https://gitlab.com/mlgepicbanana/billybot"
    This reverts commit f8f5465ddf3ec201b18d8b078252c39cf31876a0

--- resources/shitposting/shitpost.sql
+++ resources/shitposting/shitpost.sql
@@ -1,21 +1,21 @@
-CREATE SCHEMA billybot_db;
-USE billybot_db;
+CREATE SCHEMA shitposting_db;
+USE shitposting_db;
 
-CREATE TABLE sp_tags_tbl(
+CREATE TABLE tags_tbl(
 	id INT NOT NULL AUTO_INCREMENT,
 	tag VARCHAR(256) NOT NULL,
 	PRIMARY KEY(id),
 	UNIQUE KEY tag_UNIQUE (tag)
 );
 
-CREATE TABLE sp_file_extensions_tbl(
+CREATE TABLE file_extensions_tbl(
 	id INT NOT NULL AUTO_INCREMENT,
 	extension VARCHAR(4) NOT NULL,
 	PRIMARY KEY(id),
 	UNIQUE KEY extension_UNIQUE (extension)
 );
 
-CREATE TABLE sp_user_privileges_tbl(
+CREATE TABLE user_privileges_tbl(
 	id INT NOT NULL AUTO_INCREMENT,
 	name VARCHAR(16) NOT NULL,
 	owner TINYINT NULL,
@@ -35,12 +35,12 @@ CREATE TABLE sp_user_privileges_tbl(
 	CONSTRAINT query_bool CHECK (query = TRUE OR query = FALSE)
 );
 
-CREATE TABLE sp_users_tbl(
+CREATE TABLE users_tbl(
 	discord_user_id VARCHAR(18) NOT NULL,
 	privilege_id INT NOT NULL,
 	PRIMARY KEY(discord_user_id),
 	CONSTRAINT FK_privilegeID_users
-		FOREIGN KEY(privilege_id) REFERENCES sp_user_privileges_tbl(id)
+		FOREIGN KEY(privilege_id) REFERENCES user_privileges_tbl(id)
 		ON DELETE RESTRICT
 		ON UPDATE CASCADE
 );
@@ -56,23 +56,23 @@ CREATE TABLE shitposts_tbl(
 	UNIQUE KEY UNIQUE_shitpostDescription (description),
 	UNIQUE KEY UNIQUE_fileHash (file_hash),
 	CONSTRAINT FK_fileExtensionID_shitposts
-		FOREIGN KEY(file_extension_id) REFERENCES sp_file_extensions_tbl(id)
+		FOREIGN KEY(file_extension_id) REFERENCES file_extensions_tbl(id)
 		ON DELETE RESTRICT
 		ON UPDATE RESTRICT,
 	CONSTRAINT FK_submitterID_shitposts
-		FOREIGN KEY(submitter_id) REFERENCES sp_users_tbl(discord_user_id)
+		FOREIGN KEY(submitter_id) REFERENCES users_tbl(discord_user_id)
 		ON DELETE NO ACTION
 		ON UPDATE CASCADE
 );
 
-CREATE TABLE sp_shitposts_tags_tbl(
+CREATE TABLE shitposting_tags_tbl(
 	entry INT NOT NULL AUTO_INCREMENT,
 	tag_id INT NOT NULL,
 	shitpost_id INT NOT NULL,
 	PRIMARY KEY(entry),
 	UNIQUE KEY shitpost_tag_UNIQUE (tag_id, shitpost_id),
 	CONSTRAINT FK_tagID_shitpostingTags
-		FOREIGN KEY(tag_id) REFERENCES sp_tags_tbl(id)
+		FOREIGN KEY(tag_id) REFERENCES tags_tbl(id)
 		ON UPDATE CASCADE
 		ON DELETE CASCADE,
 	CONSTRAINT FK_shitpostID_shitpostingTags
@@ -81,7 +81,7 @@ CREATE TABLE sp_shitposts_tags_tbl(
 		ON DELETE CASCADE
 );
 
-CREATE TABLE sp_rating_tbl(
+CREATE TABLE rating_tbl(
 	entry INT NOT NULL AUTO_INCREMENT,
 	value TINYINT NOT NULL,
 	submitter_id VARCHAR(18) NOT NULL,
@@ -89,7 +89,7 @@ CREATE TABLE sp_rating_tbl(
 	PRIMARY KEY(entry),
 	UNIQUE KEY rate_UNIQUE (submitter_id, shitpost_id),
 	CONSTRAINT FK_submitterID_rating
-		FOREIGN KEY(submitter_id) REFERENCES sp_users_tbl(discord_user_id)
+		FOREIGN KEY(submitter_id) REFERENCES users_tbl(discord_user_id)
 		ON UPDATE CASCADE
 		ON DELETE CASCADE,
 	CONSTRAINT FK_shitpostID_rating
@@ -99,7 +99,7 @@ CREATE TABLE sp_rating_tbl(
 	CONSTRAINT value_legal_rate CHECK (value >= 0 OR value <= 100)
 );
 
-INSERT INTO sp_user_privileges_tbl
+INSERT INTO user_privileges_tbl
 (name, owner, administrator, remove, submit, rate, query) VALUES
 ("owner", TRUE, TRUE, TRUE, TRUE, TRUE, TRUE),
 ("admin", NULL, TRUE, TRUE, TRUE, TRUE, TRUE),
@@ -108,33 +108,30 @@ INSERT INTO sp_user_privileges_tbl
 ("user", NULL, FALSE, FALSE, FALSE, FALSE, TRUE),
 ("banned", NULL, FALSE, FALSE, FALSE, FALSE, FALSE);
 
-# INSERT INTO sp_users 238834017980514304
-
 /*
-INSERT INTO sp_tags_tbl (tag) VALUES
-("AMOGUS"),
-("ARANARA"),
-("BETTER_CALL_SAUL"),
-("BREAKING_BAD"),
-("CARS_MOVIE"),
-("CBT"),
-("FUMO"),
-("INTRODUCTION_MEME"),
-("LEBRON_JAMES"),
-("LOW_QUALITY"),
-("MY_CHILD_MEME"),
-("ONE_PIECE"),
-("OSU!"),
-("PIRACY"),
-("SPEEDY_MCQUEEN"),
-("THE_ONE_PIECE_IS_REAL"),
-("THREAT"),
-("TOUHOU"),
-("WONDER")
+INSERT INTO tags_tbl (tag) VALUES
+("amogus"),
+("aranara"),
+("better_call_saul"),
+("breaking_bad"),
+("cars_movie"),
+("cbt"),
+("fumo"),
+("lebron_james"),
+("low_quality"),
+("my_child_meme"),
+("one_piece"),
+("osu!"),
+("piracy"),
+("introduction_meme"),
+("speedy_mcqueen"),
+("the_one_piece_is_real"),
+("threat"),
+("touhou")
 ;
 */
 
-INSERT INTO sp_file_extensions_tbl (extension) VALUES
+INSERT INTO file_extensions_tbl (extension) VALUES
 ("png"),
 ("jpg"),
 ("jpeg"),
@@ -142,8 +139,8 @@ INSERT INTO sp_file_extensions_tbl (extension) VALUES
 ("mp3"),
 ("gif");
 
-CREATE VIEW sp_user_privileges_view AS
-SELECT sp_users_tbl.discord_user_id, id, name, owner, administrator, submit, remove, rate, query
-FROM sp_user_privileges_tbl
-INNER JOIN sp_users_tbl
-ON sp_users_tbl.privilege_id = sp_user_privileges_tbl.id;
+CREATE VIEW user_privileges_view AS
+SELECT users_tbl.discord_user_id, id, name, owner, administrator, submit, remove, rate, query
+FROM user_privileges_tbl
+INNER JOIN users_tbl
+ON users_tbl.privilege_id = user_privileges_tbl.id;
